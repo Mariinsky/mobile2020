185d60ef68f121600dcd6f38b21b1446bfcdc387
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-08-30 19:32:46 +0300

    map

--- app/src/main/java/com/example/threadingnetwork/MainActivity.kt
+++ app/src/main/java/com/example/threadingnetwork/MainActivity.kt
@@ -11,10 +11,19 @@ import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.preference.PreferenceManager
 import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.activity_main.*
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.ItemizedIconOverlay.OnItemGestureListener
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus
+import org.osmdroid.views.overlay.OverlayItem
+import org.osmdroid.views.overlay.Polyline
 import java.util.*
-import kotlin.math.roundToInt
+import kotlin.collections.ArrayList
+
 
 const val REQUEST_LOCATION_CODE = 100
 
@@ -24,29 +33,59 @@ class MainActivity : AppCompatActivity() {
     private var firstLocationSet = false
     private lateinit var firstLocation: Location
     private lateinit var geocoder: Geocoder
+    private val polyLine = Polyline()
+    private val pathPoints= ArrayList<GeoPoint>()
+    val items = ArrayList<OverlayItem>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val ctx = applicationContext
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
         setContentView(R.layout.activity_main)
         this.title = "location"
-        locationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
         geocoder = Geocoder(this, Locale.getDefault())
+        locationClient = LocationServices.getFusedLocationProviderClient(this)
+
         checkPermission()
 
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.setMultiTouchControls(true)
+        map.controller.setZoom(9.0)
+
+
         nappi.setOnClickListener {
-                locationClient.requestLocationUpdates(
-                    createLocationRequest(),
-                    locationCallback(),
-                    Looper.getMainLooper()
-                )
-            }
+            locationClient.requestLocationUpdates(
+                createLocationRequest(),
+                locationCallback(),
+                Looper.getMainLooper()
+            )
         }
+    }
 
     private fun checkPermission(): Boolean {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
         ) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION ), REQUEST_LOCATION_CODE)
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ), REQUEST_LOCATION_CODE
+            )
             return false
         }
         nappi.isEnabled = true
@@ -57,14 +96,43 @@ class MainActivity : AppCompatActivity() {
         @SuppressLint("SetTextI18n")
         override fun onLocationResult(locationResult: LocationResult?) {
             locationResult ?: return
-            for (location in locationResult.locations){
+            for (location in locationResult.locations) {
                 if (!firstLocationSet) {
                     firstLocation = location
+                    val startingPoint = GeoPoint(
+                        location.latitude,
+                        location.longitude
+                    )
+                    items.add(OverlayItem("Location", "${location.latitude} ${location.longitude}", startingPoint))
+
+                    map.controller.setCenter(
+                        startingPoint
+                    )
                     firstLocationSet = true
                 }
-                val address = geocoder.getFromLocation(location.latitude, location.longitude, 1)
-                loccis.text =  "${address[0].locality}\n${address[0].getAddressLine(0)}"
-                distance.text = "Distance from origin: ${location.distanceTo(firstLocation).roundToInt() / 1000}km"
+                val currentPoint = GeoPoint(
+                    location.latitude,
+                    location.longitude
+                )
+                val mOverlay = ItemizedOverlayWithFocus(
+                    items,
+                    object : OnItemGestureListener<OverlayItem?> {
+                        override fun onItemSingleTapUp(index: Int, item: OverlayItem?): Boolean {
+                            //do something
+                            return true
+                        }
+
+                        override fun onItemLongPress(index: Int, item: OverlayItem?): Boolean {
+                            return false
+                        }
+                    }, this@MainActivity
+                )
+                pathPoints.add(currentPoint)
+                mOverlay.setFocusItemsOnTap(true)
+                polyLine.setPoints(pathPoints)
+                map.overlays.add(polyLine)
+                map.getOverlays().add(mOverlay)
+
             }
         }
     }
@@ -87,9 +155,10 @@ class MainActivity : AppCompatActivity() {
                 PackageManager.PERMISSION_GRANTED -> nappi.isEnabled = true
                 PackageManager.PERMISSION_DENIED -> {
                     Log.i("XXX", permissions.toString())
-                    if (shouldShowRequestPermissionRationale( Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
                         showAlert(permissions, "LOCATION", REQUEST_LOCATION_CODE)
-                    }}
+                    }
+                }
             }
         }
     }
@@ -101,8 +170,8 @@ class MainActivity : AppCompatActivity() {
             setTitle("Permission needed")
             setPositiveButton("I understand") { _, _ ->
                 ActivityCompat.requestPermissions(this@MainActivity, permissions, requestCode)
-                }
             }
+        }
             .create()
             .show()
     }
