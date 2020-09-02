c6eba7eaa295c92ec764c96c7df0cf09e1f66cba
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-09-02 19:16:18 +0300

    no results

--- app/src/main/java/com/example/threadingnetwork/MainActivity.kt
+++ app/src/main/java/com/example/threadingnetwork/MainActivity.kt
@@ -4,48 +4,75 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.*
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btAdapter: BluetoothAdapter
-    private lateinit var btManager: BluetoothManager
-    private var scanResults: HashMap<String, ScanResult>? = null
-    private var scanCallBack: BtScanCallBack? = null
-    private var btScanner : BluetoothLeScanner? = null
-    private val SCAN_PERIOD: Long = 3000
+    private lateinit var bluetoothManager: BluetoothManager
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+    private lateinit var bluetoothLeScanner: BluetoothLeScanner
+    private var mScanning = false
+    private val handler = Handler()
+
+    private val SCAN_PERIOD: Long = 10000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        btAdapter = btManager.adapter
-
-    }
-
-    private fun startScan() {
-        scanResults = HashMap()
-        scanCallBack = BtScanCallBack()
-        btScanner = btAdapter.bluetoothLeScanner
-
-        val settings = ScanSettings.Builder()
-            .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
-            .build()
-
-        val filter: List<ScanFilter>? = null
-
-        val mHandler = Handler()
-        mHandler.postDelayed( {stopScan() })
+        bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothAdapter = bluetoothManager.adapter
+        bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+        askBtPermission()
+        button.setOnClickListener {
+            bluetoothLeScanner.startScan(leScanCallback)
+            scanLeDevice()
+        }
 
     }
 
-    private inner class BtScanCallBack : ScanCallback() {
-
+    private fun scanLeDevice() {
+        if (!mScanning) { // Stops scanning after a pre-defined scan period.
+            Log.i("XXX", "startScan")
+            handler.postDelayed({
+                mScanning = false
+                bluetoothLeScanner.stopScan(leScanCallback)
+            }, SCAN_PERIOD)
+            mScanning = true
+            bluetoothLeScanner.startScan(leScanCallback)
+            Log.i("XXX", "stop")
+        } else {
+            mScanning = false
+            bluetoothLeScanner.stopScan(leScanCallback)
+            Log.i("XXX", "stop2")
+        }
     }
 
+    private val leScanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+            tekstii.text = "horo"
+        }
+    }
+
+    private fun askBtPermission() {
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == -1) {
+                Log.i("XXX", requestCode.toString())
+        }
+    }
 
 }
 
