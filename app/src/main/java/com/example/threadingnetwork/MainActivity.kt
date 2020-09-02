72889a744a3932d72d4c9f32ff6184da696bde9a
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-09-02 21:08:45 +0300

    btscanner1.0

--- app/src/main/java/com/example/threadingnetwork/MainActivity.kt
+++ app/src/main/java/com/example/threadingnetwork/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.threadingnetwork
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.*
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -17,61 +22,117 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bluetoothManager: BluetoothManager
     private lateinit var bluetoothAdapter: BluetoothAdapter
     private lateinit var bluetoothLeScanner: BluetoothLeScanner
+    private var scanResults: HashMap<String, ScanResult>? = null
     private var mScanning = false
     private val handler = Handler()
 
-    private val SCAN_PERIOD: Long = 10000
+    private val SCAN_PERIOD: Long = 5000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "Ultimate BT scanner 0.1"
         bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         bluetoothAdapter = bluetoothManager.adapter
         bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
-        askBtPermission()
+        scanResults = HashMap()
+        checkPermission()
         button.setOnClickListener {
-            bluetoothLeScanner.startScan(leScanCallback)
-            scanLeDevice()
+            if (!bluetoothAdapter.isEnabled) {
+                askBtPermission()
+            } else {
+                scanLeDevice()
+            }
         }
-
     }
 
     private fun scanLeDevice() {
-        if (!mScanning) { // Stops scanning after a pre-defined scan period.
-            Log.i("XXX", "startScan")
+        if (!mScanning) {
             handler.postDelayed({
                 mScanning = false
                 bluetoothLeScanner.stopScan(leScanCallback)
+                var name = ""
+                var strenght = ""
+                scanResults?.forEach {
+                    val r = it.value
+                    name += "${r.device.address}\n"
+                    strenght += "${r.rssi} dBm\n"
+                }
+                adress.text = name
+                str.text = strenght
+                progress.visibility = View.GONE
             }, SCAN_PERIOD)
+            progress.visibility = View.VISIBLE
             mScanning = true
             bluetoothLeScanner.startScan(leScanCallback)
-            Log.i("XXX", "stop")
         } else {
             mScanning = false
             bluetoothLeScanner.stopScan(leScanCallback)
-            Log.i("XXX", "stop2")
         }
     }
 
+    private fun addResult(result: ScanResult) {
+        scanResults?.set(result.device.address, result)
+    }
+
     private val leScanCallback: ScanCallback = object : ScanCallback() {
+        @RequiresApi(Build.VERSION_CODES.R)
         override fun onScanResult(callbackType: Int, result: ScanResult) {
             super.onScanResult(callbackType, result)
-            tekstii.text = "horo"
+            addResult(result)
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                200
+            )
+            return false
+        }
+        button.isEnabled = true
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 200) {
+            when (grantResults[0]) {
+                PackageManager.PERMISSION_GRANTED -> button.isEnabled = true
+                PackageManager.PERMISSION_DENIED -> {
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        showAlert(permissions, "LOCATION", 200)
+                    }
+                }
+            }
         }
     }
 
     private fun askBtPermission() {
-        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled) {
-            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, 1)
-        }
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startActivity(enableBtIntent)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == -1) {
-                Log.i("XXX", requestCode.toString())
-        }
+    private fun showAlert(permissions: Array<String>, name: String, requestCode: Int) {
+        val builder = AlertDialog.Builder(this)
+        builder.apply {
+            setMessage("Persmission to $name is need to use this app")
+            setTitle("Permission needed")
+            setPositiveButton("I understand") { _, _ ->
+                ActivityCompat.requestPermissions(this@MainActivity, permissions, requestCode)
+                }
+            }
+            .create()
+            .show()
     }
 
 }
