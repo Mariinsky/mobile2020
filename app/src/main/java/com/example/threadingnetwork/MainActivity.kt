f9b6497ac3512f719feb66a9842e4384e12b781e
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-09-03 16:59:40 +0300

    fake heartratemonitor

--- app/src/main/java/com/example/threadingnetwork/MainActivity.kt
+++ app/src/main/java/com/example/threadingnetwork/MainActivity.kt
@@ -1,27 +1,33 @@
 package com.example.threadingnetwork
 
 import android.Manifest
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
-import android.bluetooth.le.*
+import android.bluetooth.*
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.teksiview.view.*
+import java.util.*
+import kotlin.collections.HashMap
 
 
 class MainActivity : AppCompatActivity() {
@@ -31,6 +37,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var heartRateSubject: PublishSubject<String>
+    private val compositeDisposable= CompositeDisposable()
     private var scanResults: HashMap<String, ScanResult>? = null
     private var keys = mutableListOf<String>()
     private var mScanning = false
@@ -42,9 +50,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = "Ultimate BT scanner 0.1"
+        heartRateSubject = PublishSubject.create()
         scanResults = HashMap()
         viewManager = LinearLayoutManager(this)
-        viewAdapter = MyAdapter(keys, scanResults!!)
+        viewAdapter = MyAdapter(this, keys, scanResults!!, heartRateSubject)
         recyclerView = lista.apply {
             layoutManager = viewManager
             adapter = viewAdapter
@@ -58,9 +67,16 @@ class MainActivity : AppCompatActivity() {
             if (!bluetoothAdapter.isEnabled) {
                 askBtPermission()
             } else {
+                scanResults!!.clear()
                 scanLeDevice()
             }
         }
+
+        heartRateSubject
+            .subscribe {
+                heartRate.text = "$it bpm"
+            }
+            .addTo(compositeDisposable)
     }
 
     private fun scanLeDevice() {
@@ -69,7 +85,7 @@ class MainActivity : AppCompatActivity() {
                 mScanning = false
                 bluetoothLeScanner.stopScan(leScanCallback)
                 progress.visibility = View.GONE
-                scanResults?.keys?.forEach{
+                scanResults?.keys?.forEach {
                     keys.add(it)
                 }
                 viewAdapter.notifyDataSetChanged()
@@ -148,13 +164,20 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class MyAdapter(private  val keys: MutableList<String>, private val data: HashMap<String, ScanResult>) :
+class MyAdapter(
+    private val context: Context,
+    private val keys: MutableList<String>,
+    private val data: HashMap<String, ScanResult>,
+    private val subject: PublishSubject<String>
+) :
         RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     class MyViewHolder(val v: View) : RecyclerView.ViewHolder(v)
 
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): MyViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyViewHolder {
 
         val textView = LayoutInflater.from(parent.context)
             .inflate(R.layout.teksiview, parent, false) as View
@@ -166,12 +189,75 @@ class MyAdapter(private  val keys: MutableList<String>, private val data: HashMa
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val r = data[keys[position]]
         if (r != null) {
-            if (!r.isConnectable) holder.v.isEnabled = false
-            holder.v.name.text = r.device.address
+            if (!r.isConnectable) {
+                holder.v.name.isEnabled = false
+            }
+            holder.v.name.text = "${r.device.name} ${r.device.address}"
             holder.v.dbm.text = "${r.rssi} dBm"
+            holder.v.setOnClickListener {
+                r.device.connectGatt(context, false, GattClientCallback(subject))
+            }
         }
     }
 
     override fun getItemCount() = keys.size
 }
 
+class GattClientCallback(val hearRateSubject: PublishSubject<String>): BluetoothGattCallback() {
+
+    val HEART_RATE_SERVICE_UUID = convertFromInteger(0x180D)
+    val HEART_RATE_MEASUREMENT_CHAR_UUID = convertFromInteger(0x2A37)
+    val CLIENT_CHARACTERISTIC_CONFIG_UUID = convertFromInteger(0x2902)
+
+    private fun convertFromInteger(i: Int): UUID {
+        val MSB = 0x0000000000001000L
+        val LSB = -0x7fffff7fa064cb05L
+        val value = (i and -0x1).toLong()
+        return UUID(MSB or (value shl 32), LSB)
+    }
+
+    override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+        super.onConnectionStateChange(gatt, status, newState)
+        Log.i("DBG", status.toString() + " " + newState.toString())
+        if (status == BluetoothGatt.GATT_FAILURE) {
+            Log.d("DBG", "GATT connection failure")
+            return
+        } else if (status == BluetoothGatt.GATT_SUCCESS) {
+            Log.d("DBG", "GATT connection success")
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.d("DBG", "Connected GATT service")
+                gatt?.discoverServices();
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.d("DBG", "Disconnected GATT service")
+            }
+            return
+        }
+    }
+
+    override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+        super.onServicesDiscovered(gatt, status)
+        if (status != BluetoothGatt.GATT_SUCCESS) {
+            return
+        }
+        gatt?.services?.forEach {
+            if (it.uuid == HEART_RATE_SERVICE_UUID) {
+                gatt.setCharacteristicNotification(it.characteristics[0], true)
+                val descriptor = it.characteristics[0].getDescriptor(
+                    CLIENT_CHARACTERISTIC_CONFIG_UUID
+                ).apply {
+                    value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+                }
+                gatt.writeDescriptor(descriptor)
+            }
+        }
+    }
+
+    override fun onCharacteristicChanged(
+        gatt: BluetoothGatt,
+        characteristic: BluetoothGattCharacteristic
+    ) {
+        val heartRate = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 1);
+        hearRateSubject.onNext(heartRate.toString())
+    }
+
+}
