47d153496fcfdcb112447429941456eca96bcc10
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-09-03 10:29:06 +0300

    hires photo

--- app/src/main/java/com/example/threadingnetwork/MainActivity.kt
+++ app/src/main/java/com/example/threadingnetwork/MainActivity.kt
@@ -1,30 +1,63 @@
 package com.example.threadingnetwork
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
 
     val REQUEST_IMAGE_CAPTURE = 200
+    private var absolutePath = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         take_photo_button.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)  {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+            } else {
+                startImageCapture()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             startImageCapture()
         }
     }
 
     private fun startImageCapture() {
+        val imgPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        var imageFile: File? = null
+        imageFile = File.createTempFile("temp_photo", ".jpg", imgPath )
+        absolutePath = imageFile!!.absolutePath
+        val photoURI: Uri = FileProvider.getUriForFile(this,
+            "com.example.threadingnetwork.fileprovider",
+            imageFile)
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if (intent.resolveActivity(packageManager) != null) {
+            intent.flags = Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI )
             startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
         }
     }
@@ -32,8 +65,7 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            val extras = data!!.extras
-            image_result.setImageBitmap(extras!!.get("data") as Bitmap)
+            image_result.setImageBitmap(BitmapFactory.decodeFile(absolutePath))
         }
     }
 
