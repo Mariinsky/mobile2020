503713796afb2f1e97bbc899c5aefe0a831cb50a
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-09-12 18:08:17 +0300

    folders reader

--- app/src/main/java/com/example/threadingnetwork/MainActivity.kt
+++ app/src/main/java/com/example/threadingnetwork/MainActivity.kt
@@ -1,123 +1,90 @@
 package com.example.threadingnetwork
 
-import android.content.Context
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_save.*
-import kotlinx.android.synthetic.main.fragment_save.view.*
-import kotlinx.android.synthetic.main.frament_read.view.*
+import android.app.Activity
 
-internal const val FILENAME = "myFile.txt"
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+
+import androidx.documentfile.provider.DocumentFile
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.schedulers.Schedulers.io
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private val scanFiles = PublishSubject.create<Uri>()
+    private val compositeDisposable = CompositeDisposable()
+    private var files = ""
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = "internalStorage"
-
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.root_layout, SaveFragment.newInstance(), "save")
-                .commit()
-        }
-
-        save_button.setOnClickListener {
-            val frag = supportFragmentManager.findFragmentByTag("save")
-            val manager = supportFragmentManager.beginTransaction()
-            if (frag != null && frag.isVisible) {
-                manager.replace(R.id.root_layout, frag, "save").commit()
-            } else {
-                manager
-                    .replace(R.id.root_layout, SaveFragment.newInstance(), "save")
-                    .addToBackStack(null)
-                    .commit()
-            }
-        }
+        title = "folderReader"
 
         read_button.setOnClickListener {
-            val frag = supportFragmentManager.findFragmentByTag("read")
-            val manager = supportFragmentManager  .beginTransaction()
-            if (frag != null && frag.isVisible) {
-                manager.replace(R.id.root_layout, frag, "read").commit()
+            openDirectory()
+        }
+
+        scanFiles
+            .subscribeOn(io())
+            .observeOn(io())
+            .map {
+                documentTree(it)
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                files += it
+                files_list.text = files
+            }
+            .addTo(compositeDisposable)
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun openDirectory() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+        }
+
+        startActivityForResult(intent, 1)
+    }
+
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int, resultData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, resultData)
+        if (requestCode == 1
+            && resultCode == Activity.RESULT_OK) {
+            resultData?.data?.also { uri ->
+                scanFiles.onNext(uri)
+            }
+        }
+    }
+
+    private fun documentTree(u: Uri): String {
+        val documentsTree =  DocumentFile.fromTreeUri(application, u)
+        val childDocuments = documentsTree?.listFiles()
+        var string = ""
+        childDocuments?.forEach {
+            if (it.isDirectory) {
+                string += "FOLDER: ${it.name}\n"
+                scanFiles.onNext(it.uri)
             } else {
-                manager
-                    .replace(R.id.root_layout, ReadFragment.newInstance(), "read")
-                    .addToBackStack(null)
-                    .commit()
+                string += "FILE: ${it.name}\n"
             }
         }
-
-    }
-}
-
-class SaveFragment : Fragment() {
-
-    companion object {
-        fun newInstance(): SaveFragment {
-            return SaveFragment()
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_save, container, false)
-
-        view.save_to_file_button.setOnClickListener {
-            context?.openFileOutput(FILENAME, Context.MODE_APPEND).use {
-                it?.write("${text_input.text}\n".toByteArray())
-            }
-            text_input.text.clear()
-            text_feedback.text = "File Saved"
-            hideKeyboard()
-        }
-        return view
-    }
-
-    private fun hideKeyboard() {
-        val view = activity!!.currentFocus
-        if (view != null) {
-            val inputManager: InputMethodManager =
-                activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputManager.hideSoftInputFromWindow(
-                view.windowToken,
-                InputMethodManager.HIDE_NOT_ALWAYS
-            )
-        }
-    }
-}
-
-class ReadFragment : Fragment() {
-
-    companion object {
-        fun newInstance(): ReadFragment {
-            return ReadFragment()
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.frament_read, container, false)
-        view.text_from_file.text = context?.openFileInput(FILENAME)?.bufferedReader().use {
-                it?.readText() ?: getString(R.string.read_file_failed)
-        }
-        return view
+        return string
     }
 
 }
+
 
 
 
