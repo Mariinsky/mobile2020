a90fe9d33aad4916c4af2f682036ad15dbc72247
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-09-14 10:36:55 +0300

    duck

--- app/src/main/java/com/example/threadingnetwork/MainActivity.kt
+++ app/src/main/java/com/example/threadingnetwork/MainActivity.kt
@@ -1,19 +1,93 @@
 package com.example.threadingnetwork
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.HitTestResult
+import com.google.ar.sceneform.assets.RenderableSource
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fragment: ArFragment
+    private lateinit var modelUri: Uri
+    private var testRenderable: ModelRenderable? = null
 
-
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment;
 
+        modelUri =
+            Uri.parse("https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf")
+        add_button.setOnClickListener { addObject() }
+
+        val renderableFuture = ModelRenderable.builder()
+            .setSource(
+                this, RenderableSource.builder().setSource(
+                    this,
+                    modelUri,
+                    RenderableSource.SourceType.GLTF2
+                )
+                    .setScale(0.2f) // Scale the original model to 20%.
+                    .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                    .build()
+            )
+            .setRegistryId("CesiumMan")
+            .build()
+            .thenAccept { it -> testRenderable = it }
+            .exceptionally { throwable ->
+                val toast = Toast.makeText(this, "Unable to load renderable ", Toast.LENGTH_LONG)
+                toast.setGravity(Gravity.CENTER, 0, 0)
+                toast.show()
+                null
+            }
     }
 
 
+    private fun addObject() {
+        val frame = fragment.arSceneView.arFrame
+        val pt = getScreenCenter()
+        val hits: List<HitResult>
+        if (frame != null && testRenderable != null) {
+            hits = frame.hitTest(pt.x.toFloat(), pt.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane) {
+                    val anchor = hit!!.createAnchor()
+                    val anchorNode = AnchorNode(anchor)
+                    anchorNode.setParent(fragment.arSceneView.scene)
+                    val mNode = TransformableNode(fragment.transformationSystem)
+                    mNode.setParent(anchorNode)
+                    mNode.renderable = testRenderable
+                    mNode.select()
+                    mNode.setOnTapListener { hitTestResult, motionEvent ->
+                        add_button.visibility = View.GONE
+                    }
+                    break
+                }
+            }
+        }
+    }
+
+    private fun getScreenCenter(): android.graphics.Point {
+        val vw = findViewById<View>(android.R.id.content)
+        return android.graphics.Point(vw.width / 2, vw.height / 2)
+    }
 }
 
 
